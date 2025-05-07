1fa400400d878d8c2a0caecfd3e207b893459c7c
Tofazzal Hossain <mdtofazzalislam6789@gmail.com>
2025-05-07 21:03:56 +0600

    problem one finish

--- Applo/src/index.ts
+++ Applo/src/index.ts
@@ -1,2 +1,17 @@
-const test: string = "Testing";
-console.log(test);
+// PROBLEM ----> 1
+
+function formatString(input: string, toUpper?: boolean): string {
+  if (!input) {
+    return "Privide a string";
+  }
+  if (toUpper === false) {
+    return input.toLowerCase();
+  } else {
+    return input.toUpperCase();
+  }
+}
+// formatString("Hello"); // Output: "HELLO"
+// formatString("Hello", true); // Output: "HELLO"
+// formatString("Hello", false); // Output: "hello"
+
+// PROBLEM ----> 2
