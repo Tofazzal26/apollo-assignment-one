2d8bebce0d1f5e34f00e7a37cd48797dba1a89ca
Tofazzal Hossain <mdtofazzalislam6789@gmail.com>
2025-05-07 22:45:21 +0600

    problem four solved

--- Applo/src/index.ts
+++ Applo/src/index.ts
@@ -50,3 +50,32 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 // concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
 
 // PROBLEM ----> 4
+
+class Vehicle {
+  private make: string;
+  private year: number;
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  getInfo() {
+    return `Make: ${this.make} Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+  getModel() {
+    return `Model: ${this.model}`;
+  }
+}
+
+// const myCar = new Car("Toyota", 2020, "Corolla");
+// myCar.getInfo(); // Output: "Make: Toyota, Year: 2020"
+// myCar.getModel(); // Output: "Model: Corolla"
+
+// PROBLEM ----> 5
