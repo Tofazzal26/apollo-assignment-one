1784df44bda92c264ab2234610bad34d7455bdf1
Tofazzal Hossain <mdtofazzalislam6789@gmail.com>
2025-05-07 21:27:59 +0600

    problem three solved

--- Applo/src/index.ts
+++ Applo/src/index.ts
@@ -15,3 +15,21 @@ function formatString(input: string, toUpper?: boolean): string {
 // formatString("Hello", false); // Output: "hello"
 
 // PROBLEM ----> 2
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const ratingFilter = items.filter((item) => item.rating >= 4);
+  return ratingFilter;
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+// filterByRating(books);
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+
+// PROBLEM ----> 3
